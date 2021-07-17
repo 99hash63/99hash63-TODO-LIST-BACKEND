86de2951ca53f20eea0f112b048260186cf20b07
99hash63 <thkottegoda@gmail.com>
2021-07-17 14:16:46 +0530

    created update todo endpoint with validations

--- backend/src/api/routers/todoRouter.js
+++ backend/src/api/routers/todoRouter.js
@@ -72,41 +72,42 @@ router.post("/", [
 // });
    
 
-//@route    UPDATE http://localhost:5000/UPDATE/todo/:id
+//@route    UPDATE http://localhost:5000/todo/:id
 //@desc     Update todo with a perticular ID
 //@access   public
-// router.put("UPDATE/:id", [
-//         // @validations
-//         check('timestamp', 'Invalid timestamp').trim().escape().matches(/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/),
-//         check('color', 'Invalid Color filed').not().isEmpty().trim().escape().isHexColor(),
-//         check('completed', 'Invalid completed status').not().isEmpty().isBoolean(),
-//         check('priority', 'priority is empty').not().isEmpty().trim().escape(),
-//     ],
-//     auth, async(req, res) =>{        
+router.put("/:id", [
+        // @validations
+        check('title', 'title is empty').not().isEmpty().trim().escape(),
+        check('timestamp', 'Invalid timestamp').not().isEmpty().trim().escape().matches(/^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/),
+        check('color', 'Invalid Color filed').not().isEmpty().trim().escape().isHexColor(),
+        check('completed', 'Invalid completed status').not().isEmpty().isBoolean(),
+        check('priority', 'priority is empty').not().isEmpty().trim().escape(),
+    ],
+    async(req, res) =>{        
     
-//         try{
-//             const {name, desc} = req.body;
+        try{
+            const {title, timestamp, color, completed, priority} = req.body;
 
-//             //handling request validations
-//             const error = validationResult(req);
-//             if(!error.isEmpty())
-//                 return res.status(400).json({
-//                     erroMessage: error
-//                 });
+            //handling request validations
+            const error = validationResult(req);
+            if(!error.isEmpty())
+                return res.status(400).json({
+                    erroMessage: error
+                });
 
-//             const updateCategory = {name, desc}
-//             let id = req.params.id;
-//             const updatedCategory = await Category.findByIdAndUpdate(id , updateCategory)
-//             if(!updatedCategory)
-//                 return res.status(400).json({
-//                     erroMessage: "invalid id"
-//                 });
-//             res.json({status: "Category Updated"});
-//         }catch(err){
-//             console.log(err);
-//             res.status(500).send({status: "Error with updating category", error: err.message});
-//         }
-// });
+            const updateTodo = {title, timestamp, color, completed, priority}
+            let id = req.params.id;
+            const updatedTodo = await Todo.findByIdAndUpdate(id , updateTodo)
+            if(!updatedTodo)
+                return res.status(400).json({
+                    erroMessage: "Todo not found"
+                });
+            res.json({status: "Todo Updated"});
+        }catch(err){
+            console.log(err);
+            res.status(500).send({status: "Error with updating todo", error: err.message});
+        }
+});
 
 //@route    DELETE http://localhost:5000/todo/:id
 //@desc     Dlete todo with a perticular ID
